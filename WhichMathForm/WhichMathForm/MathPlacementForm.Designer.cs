c0d3afad1e1ddd75608de7df2c66ddcb6287f91f
MistyCanCode <46390494+MistyCanCode@users.noreply.github.com>
2019-09-19 16:12:03 -0500

    Add files via upload
    initial upload of which math files

---
+++ WhichMathForm/WhichMathForm/MathPlacementForm.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace WhichMathForm
+{
+    partial class mathPlacementForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mathClassGroup = new System.Windows.Forms.GroupBox();
+            this.defaultRadioButton1 = new System.Windows.Forms.RadioButton();
+            this.finiteRadioButton = new System.Windows.Forms.RadioButton();
+            this.statisticsRadioButton = new System.Windows.Forms.RadioButton();
+            this.liberalArtsRadioButton = new System.Windows.Forms.RadioButton();
+            this.foundationsRadioButton = new System.Windows.Forms.RadioButton();
+            this.testGroup = new System.Windows.Forms.GroupBox();
+            this.defaultRadioButton2 = new System.Windows.Forms.RadioButton();
+            this.satRadioButton = new System.Windows.Forms.RadioButton();
+            this.actRadioButton = new System.Windows.Forms.RadioButton();
+            this.accuplacerRadioButton = new System.Windows.Forms.RadioButton();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.scoreTextBox = new System.Windows.Forms.TextBox();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.mathClassLabel = new System.Windows.Forms.Label();
+            this.mathClassGroup.SuspendLayout();
+            this.testGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mathClassGroup
+            // 
+            this.mathClassGroup.Controls.Add(this.defaultRadioButton1);
+            this.mathClassGroup.Controls.Add(this.finiteRadioButton);
+            this.mathClassGroup.Controls.Add(this.statisticsRadioButton);
+            this.mathClassGroup.Controls.Add(this.liberalArtsRadioButton);
+            this.mathClassGroup.Controls.Add(this.foundationsRadioButton);
+            this.mathClassGroup.Location = new System.Drawing.Point(30, 37);
+            this.mathClassGroup.Name = "mathClassGroup";
+            this.mathClassGroup.Size = new System.Drawing.Size(230, 231);
+            this.mathClassGroup.TabIndex = 0;
+            this.mathClassGroup.TabStop = false;
+            this.mathClassGroup.Text = "Math Class";
+            // 
+            // defaultRadioButton1
+            // 
+            this.defaultRadioButton1.AutoSize = true;
+            this.defaultRadioButton1.Location = new System.Drawing.Point(19, 201);
+            this.defaultRadioButton1.Name = "defaultRadioButton1";
+            this.defaultRadioButton1.Size = new System.Drawing.Size(86, 24);
+            this.defaultRadioButton1.TabIndex = 4;
+            this.defaultRadioButton1.TabStop = true;
+            this.defaultRadioButton1.Text = "Default";
+            this.defaultRadioButton1.UseVisualStyleBackColor = true;
+            this.defaultRadioButton1.Visible = false;
+            // 
+            // finiteRadioButton
+            // 
+            this.finiteRadioButton.AutoSize = true;
+            this.finiteRadioButton.Location = new System.Drawing.Point(19, 171);
+            this.finiteRadioButton.Name = "finiteRadioButton";
+            this.finiteRadioButton.Size = new System.Drawing.Size(113, 24);
+            this.finiteRadioButton.TabIndex = 3;
+            this.finiteRadioButton.TabStop = true;
+            this.finiteRadioButton.Text = "Finite Math";
+            this.finiteRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // statisticsRadioButton
+            // 
+            this.statisticsRadioButton.AutoSize = true;
+            this.statisticsRadioButton.Location = new System.Drawing.Point(19, 125);
+            this.statisticsRadioButton.Name = "statisticsRadioButton";
+            this.statisticsRadioButton.Size = new System.Drawing.Size(206, 24);
+            this.statisticsRadioButton.TabIndex = 2;
+            this.statisticsRadioButton.TabStop = true;
+            this.statisticsRadioButton.Text = "Probability and Statistics";
+            this.statisticsRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // liberalArtsRadioButton
+            // 
+            this.liberalArtsRadioButton.AutoSize = true;
+            this.liberalArtsRadioButton.Location = new System.Drawing.Point(19, 81);
+            this.liberalArtsRadioButton.Name = "liberalArtsRadioButton";
+            this.liberalArtsRadioButton.Size = new System.Drawing.Size(177, 24);
+            this.liberalArtsRadioButton.TabIndex = 1;
+            this.liberalArtsRadioButton.TabStop = true;
+            this.liberalArtsRadioButton.Text = "Math for Liberal Arts";
+            this.liberalArtsRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // foundationsRadioButton
+            // 
+            this.foundationsRadioButton.AutoSize = true;
+            this.foundationsRadioButton.Location = new System.Drawing.Point(19, 39);
+            this.foundationsRadioButton.Name = "foundationsRadioButton";
+            this.foundationsRadioButton.Size = new System.Drawing.Size(200, 24);
+            this.foundationsRadioButton.TabIndex = 0;
+            this.foundationsRadioButton.TabStop = true;
+            this.foundationsRadioButton.Text = "Foundations of Algebra";
+            this.foundationsRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // testGroup
+            // 
+            this.testGroup.Controls.Add(this.defaultRadioButton2);
+            this.testGroup.Controls.Add(this.satRadioButton);
+            this.testGroup.Controls.Add(this.actRadioButton);
+            this.testGroup.Controls.Add(this.accuplacerRadioButton);
+            this.testGroup.Location = new System.Drawing.Point(309, 37);
+            this.testGroup.Name = "testGroup";
+            this.testGroup.Size = new System.Drawing.Size(194, 231);
+            this.testGroup.TabIndex = 1;
+            this.testGroup.TabStop = false;
+            this.testGroup.Text = "Test Taken";
+            // 
+            // defaultRadioButton2
+            // 
+            this.defaultRadioButton2.AutoSize = true;
+            this.defaultRadioButton2.Location = new System.Drawing.Point(19, 170);
+            this.defaultRadioButton2.Name = "defaultRadioButton2";
+            this.defaultRadioButton2.Size = new System.Drawing.Size(86, 24);
+            this.defaultRadioButton2.TabIndex = 3;
+            this.defaultRadioButton2.TabStop = true;
+            this.defaultRadioButton2.Text = "Default";
+            this.defaultRadioButton2.UseVisualStyleBackColor = true;
+            this.defaultRadioButton2.Visible = false;
+            // 
+            // satRadioButton
+            // 
+            this.satRadioButton.AutoSize = true;
+            this.satRadioButton.Location = new System.Drawing.Point(19, 125);
+            this.satRadioButton.Name = "satRadioButton";
+            this.satRadioButton.Size = new System.Drawing.Size(65, 24);
+            this.satRadioButton.TabIndex = 2;
+            this.satRadioButton.TabStop = true;
+            this.satRadioButton.Text = "SAT";
+            this.satRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // actRadioButton
+            // 
+            this.actRadioButton.AutoSize = true;
+            this.actRadioButton.Location = new System.Drawing.Point(19, 81);
+            this.actRadioButton.Name = "actRadioButton";
+            this.actRadioButton.Size = new System.Drawing.Size(65, 24);
+            this.actRadioButton.TabIndex = 1;
+            this.actRadioButton.TabStop = true;
+            this.actRadioButton.Text = "ACT";
+            this.actRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // accuplacerRadioButton
+            // 
+            this.accuplacerRadioButton.AutoSize = true;
+            this.accuplacerRadioButton.Location = new System.Drawing.Point(19, 39);
+            this.accuplacerRadioButton.Name = "accuplacerRadioButton";
+            this.accuplacerRadioButton.Size = new System.Drawing.Size(113, 24);
+            this.accuplacerRadioButton.TabIndex = 0;
+            this.accuplacerRadioButton.TabStop = true;
+            this.accuplacerRadioButton.Text = "Accuplacer";
+            this.accuplacerRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(612, 37);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(91, 20);
+            this.scoreLabel.TabIndex = 2;
+            this.scoreLabel.Text = "Test score :";
+            // 
+            // scoreTextBox
+            // 
+            this.scoreTextBox.Location = new System.Drawing.Point(590, 76);
+            this.scoreTextBox.Name = "scoreTextBox";
+            this.scoreTextBox.Size = new System.Drawing.Size(130, 26);
+            this.scoreTextBox.TabIndex = 3;
+            // 
+            // calcButton
+            // 
+            this.calcButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.calcButton.Location = new System.Drawing.Point(568, 118);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(180, 47);
+            this.calcButton.TabIndex = 4;
+            this.calcButton.Text = "&Find Math Class";
+            this.calcButton.UseVisualStyleBackColor = false;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClearButton.Location = new System.Drawing.Point(568, 181);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(180, 35);
+            this.ClearButton.TabIndex = 5;
+            this.ClearButton.Text = "&Clear Form";
+            this.ClearButton.UseVisualStyleBackColor = false;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.ExitButton.Location = new System.Drawing.Point(568, 233);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(180, 35);
+            this.ExitButton.TabIndex = 6;
+            this.ExitButton.Text = "&Exit";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultTextBox.Location = new System.Drawing.Point(30, 368);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.Size = new System.Drawing.Size(718, 39);
+            this.resultTextBox.TabIndex = 7;
+            // 
+            // mathClassLabel
+            // 
+            this.mathClassLabel.AutoSize = true;
+            this.mathClassLabel.Location = new System.Drawing.Point(30, 306);
+            this.mathClassLabel.Name = "mathClassLabel";
+            this.mathClassLabel.Size = new System.Drawing.Size(156, 20);
+            this.mathClassLabel.TabIndex = 8;
+            this.mathClassLabel.Text = "Math Class Needed :";
+            // 
+            // mathPlacementForm
+            // 
+            this.AcceptButton = this.calcButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.ExitButton;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mathClassLabel);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.scoreTextBox);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.testGroup);
+            this.Controls.Add(this.mathClassGroup);
+            this.Name = "mathPlacementForm";
+            this.Text = "Math Placement Form";
+            this.mathClassGroup.ResumeLayout(false);
+            this.mathClassGroup.PerformLayout();
+            this.testGroup.ResumeLayout(false);
+            this.testGroup.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox mathClassGroup;
+        private System.Windows.Forms.RadioButton finiteRadioButton;
+        private System.Windows.Forms.RadioButton statisticsRadioButton;
+        private System.Windows.Forms.RadioButton liberalArtsRadioButton;
+        private System.Windows.Forms.RadioButton foundationsRadioButton;
+        private System.Windows.Forms.GroupBox testGroup;
+        private System.Windows.Forms.RadioButton satRadioButton;
+        private System.Windows.Forms.RadioButton actRadioButton;
+        private System.Windows.Forms.RadioButton accuplacerRadioButton;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.TextBox scoreTextBox;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Label mathClassLabel;
+        private System.Windows.Forms.RadioButton defaultRadioButton1;
+        private System.Windows.Forms.RadioButton defaultRadioButton2;
+    }
+}
+
---
+++ WhichMathForm/WhichMathForm/MathPlacementForm.cs
@@ -0,0 +1,255 @@
+﻿//Misty Tomlin 9-28-18 project 5 Math Class selecter
+//This program takes input from two radio buttons and a text box to
+//determine whether student can take that class or needs prerequisites
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhichMathForm
+{
+    public partial class mathPlacementForm : Form
+    {
+        public mathPlacementForm()
+        {
+            InitializeComponent();
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+
+            //constants for placement tests
+            const int Accuplacer = 92;
+            const int Act = 19;
+            const int Sat = 460;
+
+            //converts string score it an int32 for if statements
+            //int score = Convert.ToInt32(scoreTextBox.Text);
+
+            //try catch for validation 
+            try
+            {
+                int score = Convert.ToInt32(scoreTextBox.Text);
+                //this if statement checks for the foundations of algebra radio button
+                //then it tests for the test taken and the score to see if they are 
+                //clear to take foundations of algebra
+
+                //not working
+                if (foundationsRadioButton.Checked)
+                {
+                    if (accuplacerRadioButton.Checked)
+                    {
+                        if (score >= Accuplacer)
+                        {
+                            resultTextBox.Text = "Clear to take Foundations of Algebra.";
+                        }
+                        else
+                        {
+                            resultTextBox.Text = "Must take Math 1000 and Math 0815.";
+                        }
+                    }
+                }
+                else if (foundationsRadioButton.Checked)
+                {
+                    if (actRadioButton.Checked)
+                    {
+                        if (score >= Act)
+                        {
+                            resultTextBox.Text = "Clear to take Foundations of Algebra.";
+                        }
+                        else
+                        {
+                            resultTextBox.Text = "Must take Math 1000 and Math 0815.";
+                        }
+                    }
+                }
+
+                else if (foundationsRadioButton.Checked)
+                {
+                    if (satRadioButton.Checked)
+                    {
+                        if (score >= Sat)
+                        {
+                            resultTextBox.Text = "Clear to take Foundations of Algebra.";
+                        }
+
+                        else
+                        {
+                            resultTextBox.Text = "Must take Math 1000 and Math 0815.";
+                        }
+                    }
+                }
+
+
+                //this section checks for math for liberal arts and then it checks for
+                //the test taken and the score in a nested if statement
+
+                //not working
+                if (liberalArtsRadioButton.Checked)
+                {
+                    if (accuplacerRadioButton.Checked)
+                    {
+                        if (score >= Accuplacer)
+                        {
+                            resultTextBox.Text = "Clear to take Math for Liberal Arts.";
+                        }
+                        else
+                        {
+                            resultTextBox.Text = "Must take Math 1010 and Math 0825.";
+                        }
+                    }
+                }
+                else if (liberalArtsRadioButton.Checked)
+                {
+                    if (actRadioButton.Checked)
+                    {
+                        if (score >= Act)
+                        {
+                            resultTextBox.Text = "Clear to take Math for Liberal Arts.";
+                        }
+                    }
+                    else
+                    {
+                        resultTextBox.Text = "Must take Math 1010 and Math 0825.";
+                    }
+                }
+                else if (liberalArtsRadioButton.Checked)
+                {
+                    if (satRadioButton.Checked)
+                    {
+                        if (score >= Sat)
+                        {
+                            resultTextBox.Text = "Clear to take Math for Liberal Arts.";
+                        }
+                    }
+                    else
+                    {
+                        resultTextBox.Text = "Must take Math 1010 and Math 0825.";
+                    }
+                }
+
+
+                //nested ifs for probability and statistics, and test and score
+
+                //not working
+                if (statisticsRadioButton.Checked)
+                {
+                    if (accuplacerRadioButton.Checked)
+                    {
+                        if (score >= Accuplacer)
+                        {
+                            resultTextBox.Text = "Clear to take Probability and Statistics.";
+                        }
+                    }
+                    else
+                    {
+                        resultTextBox.Text = "Must take Math 1530 & Math 0835.";
+                    }
+                }
+                else if (statisticsRadioButton.Checked)
+                {
+                    if (actRadioButton.Checked)
+                    {
+                        if (score >= Act)
+                        {
+                            resultTextBox.Text = "Clear to take Probability and Statistics.";
+                        }
+                    }
+                    else
+                    {
+                        resultTextBox.Text = "Must take Math 1530 & Math 0835.";
+                    }
+                }
+                else if (statisticsRadioButton.Checked)
+                {
+                    if (satRadioButton.Checked)
+                    {
+                        if (score >= Sat)
+                        {
+                            resultTextBox.Text = "Clear to take Probability and Stetistics.";
+                        }
+                    }
+                    else
+                    {
+                        resultTextBox.Text = "Must take Math 1530 & Math 0835.";
+                    }
+                }
+
+
+                //nested ifs for finite math, and test and score
+                //working
+                if (finiteRadioButton.Checked)
+                {
+                    if (accuplacerRadioButton.Checked)
+                    {
+                        if (score >= Accuplacer)
+                        {
+                            resultTextBox.Text = "Clear to take Finite Math.";
+                        }
+                    }
+                    else
+                    {
+                        resultTextBox.Text = "Must take Math 1630 & Math 0845.";
+                    }
+                }
+                else if (finiteRadioButton.Checked)
+                {
+                    if (actRadioButton.Checked)
+                    {
+                        if (score >= Act)
+                        {
+                            resultTextBox.Text = "Clear to take Finite Math.";
+                        }
+                    }
+                    else
+                    {
+                        resultTextBox.Text = "Must take Math 1630 & Math 0845.";
+                    }
+                }
+                else if (finiteRadioButton.Checked)
+                {
+                    if (satRadioButton.Checked)
+                    {
+                        if (score >= Sat)
+                        {
+                            resultTextBox.Text = "Clear to take Finite Math.";
+                        }
+                    }
+                    else
+                    {
+                        resultTextBox.Text = "Must take Math 1630 & Math 0845.";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            scoreTextBox.Text = "";
+            resultTextBox.Text = "";
+            defaultRadioButton1.Checked = true;
+            defaultRadioButton2.Checked = true;
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
+
+        
+    
+
